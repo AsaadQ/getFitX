334bf15b2d064ea2a2898c8b67530bf5f1ac5744
Asaad Qudsieh <asadmoaed@yahoo.com>
2021-04-25 13:36:42 +0200

    v4

--- matplan/url.py
+++ matplan/url.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='khara'), #dette het home
-    path('kalori', views.calc, name='home'),#dette het calc
+    path('', views.Planner, name='Plans'),
 ]
---
+++ matplan/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+# Create your views here.
+
+def Planner(request):
+    return render(request, 'MatPlan/Planner.html')
