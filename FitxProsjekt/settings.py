b489b53488a265bf70e044ee0ac69d1135e43cc4
Asaad Qudsieh <asadmoaed@yahoo.com>
2021-05-20 00:32:21 +0200

    database

--- FitxProsjekt/settings.py
+++ FitxProsjekt/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$fp=f#2d&%ayr@v^%f!%5rqvc_6@d-jx-p_^*g@=x_j=*9q)r('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = []
 
 # Application definition
