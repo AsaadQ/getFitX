20d66f3f1c27f2d16f0f1b6ded8f2284066de44d
Asaad Qudsieh <asadmoaed@yahoo.com>
2021-05-19 16:15:02 +0200

    commit

--- FitxProsjekt/settings.py
+++ FitxProsjekt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -193,3 +194,5 @@ LOGGING = {
         },
     }
 }
+# Activate Django-Heroku.
+django_heroku.settings(locals())
