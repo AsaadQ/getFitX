f50051dfa59de93f6e0bc901e4b37212de2afabe
Asaad Qudsieh <asadmoaed@yahoo.com>
2021-05-19 14:50:10 +0200

    commit

--- FitxProsjekt/urls.py
+++ FitxProsjekt/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf.urls.i18n import i18n_patterns
+from django.views import static
 from django.views.static import serve
 
 from accounts import views
@@ -32,3 +33,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+urlpatterns=urlpatterns+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
