853d20e303b169c8a6b780c04512165396d1be8e
Asaad Qudsieh <asadmoaed@yahoo.com>
2021-05-19 21:17:20 +0200

    commit

--- FitxProsjekt/settings.py
+++ FitxProsjekt/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 import django_heroku
 import dj_database_url
-from decouple import config
+import decouple
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
